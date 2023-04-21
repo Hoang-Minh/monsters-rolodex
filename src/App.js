6ea607205c1acf99815cf0092c2c5a86b62cbd45
Minh Nguyen <minh.nguyen@ocit.ocgov.com>
2023-04-21 14:47:47 -0700

    finished app with class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -31,34 +33,21 @@ class App extends Component {
     this.setState(() => {
       return { searchField };
     });
-  }
+  };
 
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
 
-    const {monsters, searchField} = this.state;
-    const {onSearchChange} = this;
-
-    const filteredMonsters = monsters.filter((monster) => {    
-      
+    const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
-    });    
+    });
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
-        ></input>
-
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox onChangeHandler={onSearchChange} placeHolder="Search Monsters" className="monsters-search-box"></SearchBox>
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
